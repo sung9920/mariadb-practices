b246e0fb3817a9cb5bc36f23f7bc607de8a71077
sung9920 <sungcheol9920@gmail.com>
2025-08-19 15:08:33 +0900

    jdbc #1

---
+++ jdbc-practices/src/main/java/example/UpdateEx02.java
@@ -0,0 +1,14 @@
+package example;
+
+public class UpdateEx02 {
+
+	public static void main(String[] args) {
+		update(1L, "경영지원팀");
+	}
+
+	private static void update(long id, String name) {
+		
+		
+	}
+	
+}
