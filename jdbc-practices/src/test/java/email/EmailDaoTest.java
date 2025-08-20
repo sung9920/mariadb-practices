f73fa72bab8d6e6ed3c6c6e9ef04a5dfa18cfd1d
sung9920 <sungcheol9920@gmail.com>
2025-08-20 10:02:43 +0900

    email # test

---
+++ jdbc-practices/src/test/java/email/EmailDaoTest.java
@@ -0,0 +1,17 @@
+package email;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class EmailDaoTest {
+	
+	@Test
+	public void test() {
+		int result = 10 - 2;
+		
+		assertEquals(0, result);
+		
+	}
+
+}
