9eb129b12c0a7bea3508a31a419a3fcf8a0adcb8
sung9920 <sungcheol9920@gmail.com>
2025-08-21 09:34:02 +0900

    email #finish

--- jdbc-practices/sql/webdb.sql
+++ jdbc-practices/sql/webdb.sql
@@ -23,8 +23,6 @@ ALTER TABLE dept AUTO_INCREMENT = 8;
 -- email application
 -- 
 
-desc email;
-
 -- findAll
 select id, first_name, last_name, email from email order by id desc;
 
@@ -33,3 +31,26 @@ delete from email where email = 'dooly@gmail.com';
 
 -- insert
 insert into email(first_name, last_name, email) values ( '둘', '리', 'dooly@gmail.com');
+
+--
+-- bookshop application
+--
+
+-- select
+select * from author;
+select * from book;
+
+-- deleteAll
+delete from book;
+delete from author;
+
+-- insert
+insert into author(name) values ('박성철');
+insert into book(title,author_id) values ('제목1', 2);
+
+-- findAll
+select a.id, a.title, b.name, a.status from book a, author b where a.author_id = b.id;
+
+-- update
+update book set status = '대여중' where id = 1;
+
